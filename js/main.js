29a3d3633593c958d698aa189488baa2638f03b8
Parth Patel <parthcdgsa@gmail.com>
2018-11-24 12:54:32 +0530

    send post requests

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 $(()=>{
     // addMessages({name:"Parth", message:"Hi, there"});
     clickSend();
+    getMessages();
 });
 
 function addMessages(message){
     $('#messages').append(`<h4> ${message.name} </h4> <p> ${message.message} </p>`);
+}
+
+function getMessages(){
+    $.get('http://localhost:3000/messages', (data)=>{
+        data.forEach(addMessages);
+    });
 }
 
 function clickSend(){
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 // app.listen(3000,'localhost',(err)=>{
@@ -6,6 +7,22 @@ var app = express();
 // });
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+
+var messages = [
+    {name:'Tom', message: 'Hi, I am Tim'},
+    {name:'Jerry', message: 'Hi, I am Jerry'}
+];
+
+app.get('/messages', (req,res)=>{
+    res.send(messages);
+});
+
+app.post('/messages', (req,res)=>{
+    messages.push(req.body);
+    res.sendStatus(200);
+});
+
 var server = app.listen(3000, ()=>{
     console.log(`server is listening on port`, server.address().port);
 });
