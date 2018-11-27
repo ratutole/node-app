7617927678a112e4b35836e6b3c3ef0b8a70c119
Parth Patel <parthcdgsa@gmail.com>
2018-11-27 18:38:23 +0530

    finishing up

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 // app.listen(3000,'localhost',(err)=>{
 //     console.log(`Server Started`, err);
@@ -12,21 +13,50 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-var messages = [
-];
+mongoose.Promise = Promise;
+
+dbURL = "mongodb://ratshere:ratshere2@ds013330.mlab.com:13330/learn-node";
+
+var Message = mongoose.model('Message',{
+    name: String,
+    message: String,
+});
+
 
 app.get('/messages', (req,res)=>{
-    res.send(messages);
+    Message.find({},(err,messages)=>{
+
+        res.send(messages);
+    });
 });
 
 app.post('/messages', (req,res)=>{
-    messages.push(req.body);
-    io.emit('message',req.body);
-    res.sendStatus(200);
+    var message = new Message(req.body);
+    message.save().then(() => {
+        Message.findOne({message:"badword"},(err,censored)=>{
+            if(censored){
+                console.log('censored words found');
+                Message.remove({_id: censored.id}, (err)=>{
+                    console.log("removed censored message");
+                });
+            }
+        });
+        io.emit('message',req.body);
+        res.sendStatus(200);
+    }).catch((err)=>{
+        res.sendStatus(500);
+        return console.error(err);
+    });
+
+    
 });
 
 io.on('connection',(socket)=>{
     console.log('user connected');
+});
+
+mongoose.connect(dbURL,{useNewUrlParser:true},(err)=>{
+    console.log('mongodb connection', err);
 });
 
 var server = http.listen(3000, ()=>{
---
+++ spec/server.spec.js
@@ -0,0 +1,6 @@
+describe('calc',() => {
+    it("should multiply 2 and 2",() => {
+        expect(2*2).toBe(4);
+    });
+});
+
